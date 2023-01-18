09e3c756748596c1e3d17f5bfcd00b302ff379cd
petersamson-kingori <peters.kingori@student.moringaschool.com>
2023-01-19 00:54:44 +0300

    Javascript linked file updated

---
+++ index.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    console.log("Hello World");
+
+    Hello = document.getElementById('Title')
+    Hello.textContent = "Hello World"
+
+    
+})
