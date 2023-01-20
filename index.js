75619890cc678e73ebf718c4ea7aa89f3e0fa1f3
petersamson-kingori <peters.kingori@student.moringaschool.com>
2023-01-20 14:38:04 +0300

    database link updated

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded",()=>{
     showMovies()
 })
 function showMovies(){
-    fetch("")
+    fetch("https://my-json-server.typicode.com/petersamson-kingori/phase1-project/location")
   .then(response => response.json())
   .then(location => { 
     console.log(location)})}
