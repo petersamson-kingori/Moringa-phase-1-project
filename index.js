96f6fa7379021213a5e1388ea0adfc12c97cd0ed
petersamson-kingori <peters.kingori@student.moringaschool.com>
2023-01-20 19:00:05 +0300

    HTML and CSS UPDATED

--- index.js
+++ index.js
@@ -2,11 +2,30 @@
 // Your code here
 
 document.addEventListener("DOMContentLoaded",()=>{
-    console.log("Content Loaded bro");
+  console.log("You got this bro")
     showMovies()
 })
+
+
+
+
+
+
+
 function showMovies(){
-    fetch("https://my-json-server.typicode.com/petersamson-kingori/Moringa-phase-1-project/location")
+  fetch('https://my-json-server.typicode.com/petersamson-kingori/Moringa-phase-1-project/location') // fetch the json data
   .then(response => response.json())
-  .then(location => { 
-    console.log(location)})}
+  .then(data => {
+    locations = data;
+    const Title = document.getElementById('img-title')
+    Title.textContent = locations[0].title
+    const img = document.getElementById("image");
+    img.src ="https://iili.io/HcYXqJI.jpg"
+    const description = document.getElementById("img-description")
+    description.textContent = locations[0].description 
+    const title = document.getElementById("Title")
+    title.textContent = locations.location[0].title;
+  }
+  ) 
+  
+}
