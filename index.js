73e00209a0dc94b82d8bb1f14fb318f6a70a8c5a
petersamson-kingori <peters.kingori@student.moringaschool.com>
2023-01-20 12:00:45 +0300

    fetch

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 document.addEventListener('DOMContentLoaded',()=>{
-    console.log("Hello World");
+
+    fetch('https://my-json-server.typicode.com/petersamson-kingori/Moringa-phase-1-project')
+    .then((response) => response.json())
+    .then((location) => console.log(location));
+    
 
     Hello = document.getElementById('Title')
     Hello.textContent = "Hello World"
+    
+    Title = document.getElementById("img-title")
+    
 
     
 })
